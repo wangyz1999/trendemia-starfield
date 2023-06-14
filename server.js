24e86b9a7a697c8c8b492c8e1454a5b262ebe9a2
Yunzhe Wang <wangyz1999@hotmail.com>
2023-06-14 17:18:18 -0400

    update

--- server.js
+++ server.js
@@ -8,12 +8,11 @@ app.use(express.static(__dirname + '/dist/research-dym'));
 
 // Send all requests to Angular app
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/research-dym/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/research-dym/index.html'));
 });
 
-// Default Heroku port or your custom port and log message
+// Default Heroku port or your custom port and log message in a fancy way
 app.listen(process.env.PORT || 8080, () => {
-    console.log('Server started');
-    });
-
+    console.log(`App running on port ${process.env.PORT || 8080}`);
+});
 
