93146aa452c0555deb5e36c0fe3521f3da44dc2b
Yunzhe Wang <wangyz1999@hotmail.com>
2023-06-22 23:35:15 -0400

    add meta

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'research-dym'`, () => {
+  it(`should have as title 'trendemia'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('research-dym');
+    expect(app.title).toEqual('trendemia');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('research-dym app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('trendemia app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'research-dym';
+export class AppComponent implements OnInit {
+  title = 'trendemia';
+  constructor(private metaService: Meta, private titleService: Title) { }
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Trendemia Visualization');
+    this.metaService.updateTag({ name: 'author', content: 'Yunzhe Wang' });
+    this.metaService.updateTag({ name: 'description', content: 'An AI-powered research analysis tool that performs comprehensive meta-analysis and sophisticated data visualization to predict, analyze, and illustrate trends in research and technological advancements.' });
+    this.metaService.updateTag({ name: 'keywords', content: 'Data Visulization, Arxiv Paper, Research, Trend Analysis, Angular' });
+  }
 }
